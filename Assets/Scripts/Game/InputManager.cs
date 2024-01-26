27383b08d607f5f4cc807409ef050634f87251f5
Xwilarg <xwilarg@protonmail.com>
2024-01-26 11:52:35 +0100

    Add drag input

--- Assets/Scripts/Game/InputManager.cs
+++ Assets/Scripts/Game/InputManager.cs
@@ -13,13 +13,36 @@ namespace KatsisMiniGames.Engine.Game
             Instance = this;
         }
 
-        public UnityEvent OnClick { get; } = new();
+        private bool _isClicked;
+        private Vector2 _lastDragPos = Vector2.zero;
+        public UnityEvent<bool> OnClick { get; } = new();
+        public UnityEvent<Vector2> OnDrag { get; } = new();
 
         public void OnClickInternal(InputAction.CallbackContext value)
         {
-            if (value.performed)
+            if (value.phase == InputActionPhase.Started)
             {
-                OnClick.Invoke();
+                _isClicked = true;
+                _lastDragPos = Mouse.current.position.ReadValue();
+                OnClick.Invoke(true);
+            }
+            else if (value.phase == InputActionPhase.Canceled)
+            {
+                _isClicked = false;
+                OnClick.Invoke(false);
+            }
+        }
+
+        public void OnMouseMoveInternal(InputAction.CallbackContext _)
+        {
+            if (_isClicked)
+            {
+                var delta = Mouse.current.position.ReadValue() - _lastDragPos;
+
+                if (delta.magnitude != 0f)
+                {
+                    OnDrag.Invoke(delta);
+                }
             }
         }
     }
